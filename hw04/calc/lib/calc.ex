cc95f5b847663c533536d959086d9d25f8597d36
Michael Trichilo <michaeltrichilo3@gmail.com>
2018-02-03 16:31:55 -0500

    Added test cases and modified logic

--- hw04/calc/lib/calc.ex
+++ hw04/calc/lib/calc.ex
@@ -4,86 +4,17 @@ defmodule Calc do
   """
 
   @doc """
-  Entry point.
-
-  ## Examples
-
-    > 9 + 13
-    22
-
-    > 24 / 6 + (5 - 4)
-    5 
-
+  Gets an arithmatic expression from the user, evaluates it,
+  and prints the answer. This is done indefinitely.
   """
   def main() do
     IO.gets("> ") |> Calc.eval |> IO.puts
     main
   end
 
-  def pop_until([], acc, _) do
-    {[], acc}
-  end
-
-  def pop_until(stack, acc, stop_on) do
-    {pop, new_stack} = List.pop_at(stack, -1) 
-    cond do
-      pop == "(" -> {new_stack, acc}
-      Enum.member?(stop_on, pop) -> {stack, acc}
-      true -> pop_until(new_stack, acc ++ [pop], stop_on)
-    end
-  end
-
-  def postfix([], [], acc) do
-    acc
-  end
-
-  def postfix([], stack, acc) do
-    {pop, stack} = List.pop_at(stack, -1)
-    postfix([], stack, acc ++ [pop])
-  end
-
-  def postfix(expr, stack, acc) do
-    [head | tail] = expr
-    {stack, acc} =
-      case head do
-        "(" -> {stack, acc}
-        ")" -> pop_until(stack, acc, [])
-        "+" -> pop_until(stack, acc, ["+", "-"])
-        "-" -> pop_until(stack, acc, ["+", "-"])
-        "*" -> pop_until(stack, acc, ["+", "-", "*", "/"])
-        "/" -> pop_until(stack, acc, ["+", "-", "*", "/"])
-        n -> {stack, acc ++ [n]}
-      end
-    
-    case head do
-      ^head when head in ["(", "+", "-", "*", "/"] -> postfix(tail, stack ++ [head], acc)
-      _ -> postfix(tail, stack, acc)
-    end
-  end
-
-  def helper(stack, f) do
-    {second, stack} = List.pop_at(stack, -1)
-    {first, stack} = List.pop_at(stack, -1)
-    stack ++ [f.(first, second)]
-  end
-
-  def calc([], stack) do
-    hd stack 
-  end
-
-  def calc(expr, stack) do
-    [head | tail] = expr
-    stack =
-      case head do
-        "+" -> helper(stack, &+/2)
-        "-" -> helper(stack, &-/2)
-        "*" -> helper(stack, &*/2)
-        "/" -> helper(stack, &div/2)
-        n -> stack ++ [String.to_integer(n)]
-      end
-    calc(tail, stack)
-  end
-
+  @doc """
+  Parses and evaluates an arithmatic expression.
+  """
   def eval(expr) do
     String.trim(expr)
     |> String.replace("(", "( ")
@@ -91,5 +22,77 @@ defmodule Calc do
     |> String.split(" ")
     |> postfix([], [])
     |> calc([])
+  end
+  
+  # Pops elements from the given stack and adds them to the
+  # given accumulator until a stopping condition is met.
+  defp pop_until([], acc, _, _) do
+    {[], acc}
+  end
+
+  defp pop_until(stack, acc, assoc, lower) do
+    {pop, new_stack} = List.pop_at(stack, -1) 
+    cond do
+      Enum.member?(assoc, pop) -> {new_stack, acc ++ [pop]}
+      Enum.member?(lower, pop) -> {stack, acc}
+      pop == "(" -> {new_stack, acc} # Head must be a closing parentheses.
+      true -> pop_until(new_stack, acc ++ [pop], assoc, lower)
+    end
+  end
+
+  # Transforms the given infix expression into its equivalent
+  # postfix expression.
+  defp postfix([], [], acc) do
+    acc
+  end
+
+  defp postfix([], stack, acc) do
+    {pop, stack} = List.pop_at(stack, -1)
+    postfix([], stack, acc ++ [pop])
+  end
+
+  defp postfix(expr, stack, acc) do
+    [head | tail] = expr
+    {stack, acc} =
+      case head do
+        "(" -> {stack, acc}
+        ")" -> pop_until(stack, acc, [], [])
+        "+" -> pop_until(stack, acc, ["+", "-"], ["("])
+        "-" -> pop_until(stack, acc, ["+", "-"], ["("])
+        "*" -> pop_until(stack, acc, ["*", "/"], ["+", "-", "("])
+        "/" -> pop_until(stack, acc, ["*", "/"], ["+", "-", "("])
+        n -> {stack, acc ++ [n]}
+      end
+    
+    case head do
+      head when head in ["(", "+", "-", "*", "/"] -> postfix(tail, stack ++ [head], acc)
+      _ -> postfix(tail, stack, acc)
+    end
+  end
+
+  # Pops two elements from the stack, applies the given function
+  # on them, and pushes that result back onto the stack.
+  defp pop_two(stack, f) do
+    {second, stack} = List.pop_at(stack, -1)
+    {first, stack} = List.pop_at(stack, -1)
+    stack ++ [f.(first, second)]
+  end
+
+  # Evaluates a given postfix expression.
+  defp calc([], stack) do
+    hd stack 
+  end
+
+  defp calc(expr, stack) do
+    [head | tail] = expr
+    stack =
+      case head do
+        "+" -> pop_two(stack, &+/2)
+        "-" -> pop_two(stack, &-/2)
+        "*" -> pop_two(stack, &*/2)
+        "/" -> pop_two(stack, &div/2)
+        n -> stack ++ [String.to_integer(n)]
+      end
+    calc(tail, stack)
   end
 end
--- hw04/calc/test/calc_test.exs
+++ hw04/calc/test/calc_test.exs
@@ -2,7 +2,55 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "greets the world" do
-    assert Calc.hello() == :world
+  test "numbers" do
+    assert Calc.eval("0") == 0
+    assert Calc.eval("1") == 1
+    assert Calc.eval("-3") == -3
+    assert Calc.eval("15") == 15
+    assert Calc.eval("4572") == 4572
+    assert Calc.eval("-23459") == -23459
+  end
+
+  test "simple arithmatic" do
+    assert Calc.eval("1 + 1") == 2
+    assert Calc.eval("20 + 43") == 63
+    assert Calc.eval("5 - 2") == 3
+    assert Calc.eval("64 - 18") == 46
+    assert Calc.eval("3 * 3") == 9
+    assert Calc.eval("12 * 12") == 144
+    assert Calc.eval("8 / 2") == 4
+    assert Calc.eval("-25 / 4") == -6
+    assert Calc.eval("90 / 9") == 10
+  end
+
+  test "mixed operators" do
+    assert Calc.eval("3 + 8 + -9") == 2
+    assert Calc.eval("2 + 3 - -7") == 12
+    assert Calc.eval("4 + 0 * 5") == 4
+    assert Calc.eval("5 + 8 / 2") == 9
+    assert Calc.eval("10 - 6 - 1") == 3
+    assert Calc.eval("19 - 3 + 8") == 24
+    assert Calc.eval("14 - 4 * 5") == -6
+    assert Calc.eval("12 - 9 / 2") == 8
+    assert Calc.eval("13 * 2 + 1") == 27
+    assert Calc.eval("15 * 3 - 15") == 30
+    assert Calc.eval("6 * 6 * 2") == 72
+    assert Calc.eval("4 * 20 / 2") == 40
+    assert Calc.eval("16 / 4 + 5") == 9
+    assert Calc.eval("20 / 5 - 6") == -2
+    assert Calc.eval("100 / -5 * -1") == 20
+    assert Calc.eval("50 / 5 / 2") == 5
+  end
+
+  test "parentheses" do
+    assert Calc.eval("(4 + 0) * 5") == 20
+    assert Calc.eval("(5 + 8) / 2") == 6
+    assert Calc.eval("10 - (6 - 1)") == 5
+    assert Calc.eval("19 - (3 + 8)") == 8
+    assert Calc.eval("(14 - 4) * 5") == 50
+    assert Calc.eval("(12 - 9) / 2") == 1
+    assert Calc.eval("3 + 3 * (2 - 3)") == 0
+    assert Calc.eval("(2 * 0) - (3 + 9)") == -12
+    assert Calc.eval("(2 * (3 + 7 + (3 - 2)) - (2 + 7))") == 13
   end
 end
