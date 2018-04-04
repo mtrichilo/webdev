94121e0efaef627d78a6549db623dd409898c3d8
Michael Trichilo <michaeltrichilo3@gmail.com>
2018-04-04 17:52:15 -0400

    Added api

---
+++ tasks_v3/assets/js/api.js
@@ -0,0 +1,29 @@
+import store from './store';
+
+class TasksApi {
+  request(path, method, success) {
+    $.ajax(path, {
+      method: method,
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      success: success,
+    });
+  }
+
+  get(path, success) {
+    request(path, "get", success);
+  }
+
+  post(path, success) {
+    request(path, "post", success);
+  }
+
+  getTasks() {
+    get("/api/v3/tasks", (resp) => {
+      store.dispatch({
+        type: 'TASKS_LIST',
+        tasks: resp.data,
+      });
+    });
+  }
+}
